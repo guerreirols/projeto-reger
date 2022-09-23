26452263b33c8863d391ffc9751532304e396fd1
Guerreiro <luguerreiro.souza@gmail.com>
2022-09-23 02:12:22 -0300

    pagina home

--- src/assets/home/home.jsx
+++ src/assets/home/home.jsx
@@ -4,9 +4,31 @@ import './home.css'
 function Home(){
     return(
         <body>
-            <img class = 'sombra' src='src\imagens\SOMBRA.png'/>
+            <img class = 'sombra'/>
             <div>
-                <img src=''/>
+                <img class = 'logo' src='src\imagens\REGER-CAPA.png'/>
+                <nav class = 'menu'>  
+                    <ul>  
+                        <li class = 'botao'>  
+                            <input class = 'bola' type="image" src="src\imagens\BOTAO1.png" />                     
+                        </li>  
+                        <li class = 'botao'>  
+                            <input class = 'bola' type="image" src="src\imagens\BOTAO2.png"/>  
+                        </li>  
+                        <li class = 'botao'>  
+                            <input class = 'bola' type="image" src="src\imagens\BOTAO3.png"/>  
+                        </li>  
+                        <li class = 'botao'> 
+                            <input class = 'bola' type="image" src="src\imagens\BOTAO4.png"/>  
+                        </li>  
+                        <li class = 'botao'>  
+                            <input class = 'bola' type="image" src="src\imagens\BOTAO5.png"/>  
+                        </li>  
+                        <li class = 'botao'>  
+                            <input class = 'bola' type="image" src="src\imagens\BOTAO6.png"/>  
+                        </li> 
+                    </ul>  
+                </nav>  
             </div>
         </body>
     )
