65b295a09760c61274ca764bcf8e2406ad7ec834
Guerreiro <luguerreiro.souza@gmail.com>
2022-10-05 02:00:45 -0300

    odeio css

--- src/assets/paginas/home/home.jsx
+++ src/assets/paginas/home/home.jsx
@@ -3,11 +3,41 @@ import './home.css'
 function Home() {
     return (
       <>
-        <body class = "corpo-home">
+        <div class = "corpo-home">
+            <div>
+               <img class = "imagem-volume" src='src\assets\imagens\volume.png'/>
+            </div>          
             <div class = "menu-home">
-
+                <nav class = "menu">
+                    <ul>
+                        <li class = 'icone'>  
+                            <input class = 'bola' type="image" src="src\assets\imagens\folha.png" />   
+                            <p class = "titulo-icone">O Projeto</p>                  
+                        </li>  
+                        <li class = 'icone'>  
+                            <input class = 'bola' type="image" src="src\assets\imagens\folha.png" />  
+                            <p class = "titulo-icone">O Projeto</p> 
+                        </li>  
+                        <li class = 'icone'>  
+                            <input class = 'bola' type="image" src="src\assets\imagens\folha.png" />  
+                            <p class = "titulo-icone">O Projeto</p> 
+                        </li>  
+                        <li class = 'icone'>  
+                            <input class = 'bola' type="image" src="src\assets\imagens\folha.png" />  
+                            <p class = "titulo-icone">O Projeto</p>                    
+                        </li>  
+                        <li class = 'icone'>  
+                            <input class = 'bola' type="image" src="src\assets\imagens\folha.png" /> 
+                            <p class = "titulo-icone">O Projeto</p>  
+                        </li>  
+                        <li class = 'icone'>  
+                            <input class = 'bola' type="image" src="src\assets\imagens\folha.png" />  
+                            <p class = "titulo-icone">O Projeto</p>
+                        </li>  
+                    </ul>
+                </nav>            
             </div>
-        </body>
+        </div>
       </>
     )
   }
