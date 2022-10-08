2e4ded2a2581b53a6c29d5bf1ca615eafd6b66d9
Guerreiro <luguerreiro.souza@gmail.com>
2022-10-08 14:38:21 -0300

    menu alinhado e secoes adicionadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css'
+import './secoes.css'
 import Home from './assets/paginas/home/home'
+import Oprojeto from './assets/paginas/o_projeto/o_projeto'
+import Oquefazemos from './assets/paginas/o_que_fazemos/o_que_fazemos'
+import Comofazemos from './assets/paginas/como_fazemos/como_fazemos'
+import Crescemos from './assets/paginas/crescemos/crescemos'
+import Ondeestamos from './assets/paginas/onde_estamos/onde_estamos'
+import Dicas from './assets/paginas/dicas/dicas'
 
 function App() {
   return (
     <>
       <main>
         <Home/>
+        <Oprojeto/>
+        <Oquefazemos/>
+        <Comofazemos/>
+        <Crescemos/>
+        <Ondeestamos/>
+        <Dicas/>
       </main>
     </>
   )
--- src/assets/paginas/home/home.jsx
+++ src/assets/paginas/home/home.jsx
@@ -3,40 +3,45 @@ import './home.css'
 function Home() {
     return (
       <>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=ABeeZee&display=swap');
+        </style>
+        
         <div class = "corpo-home">
             <div>
                <img class = "imagem-volume" src='src\assets\imagens\volume.png'/>
             </div>          
             <div class = "menu-home">
-                <nav class = "menu">
-                    <ul>
-                        <li class = 'icone'>  
-                            <input class = 'bola' type="image" src="src\assets\imagens\folha.png" />   
-                            <p class = "titulo-icone">O Projeto</p>                  
-                        </li>  
-                        <li class = 'icone'>  
-                            <input class = 'bola' type="image" src="src\assets\imagens\folha.png" />  
-                            <p class = "titulo-icone">O Projeto</p> 
-                        </li>  
-                        <li class = 'icone'>  
-                            <input class = 'bola' type="image" src="src\assets\imagens\folha.png" />  
-                            <p class = "titulo-icone">O Projeto</p> 
-                        </li>  
-                        <li class = 'icone'>  
-                            <input class = 'bola' type="image" src="src\assets\imagens\folha.png" />  
-                            <p class = "titulo-icone">O Projeto</p>                    
-                        </li>  
-                        <li class = 'icone'>  
-                            <input class = 'bola' type="image" src="src\assets\imagens\folha.png" /> 
-                            <p class = "titulo-icone">O Projeto</p>  
-                        </li>  
-                        <li class = 'icone'>  
-                            <input class = 'bola' type="image" src="src\assets\imagens\folha.png" />  
-                            <p class = "titulo-icone">O Projeto</p>
-                        </li>  
-                    </ul>
-                </nav>            
-            </div>
+                <ul class = "menu">
+                    <li class = 'icone'>                              
+                        <input class = "imagem-icone" type="image" src="src\assets\imagens\folha.png"/>
+                        <h4 class = "titulo-icone" >O Projeto</h4>
+                    </li>  
+                    <li class = 'icone'>  
+                        <input class = "imagem-icone" type="image" src="src\assets\imagens\folha.png" />  
+                        <h4 class = 'titulo-icone' >O Que Fazemos</h4>
+                    </li>  
+                    <li class = 'icone'>  
+                        <input class = "imagem-icone" type="image" src="src\assets\imagens\folha.png" /> 
+                        <h4 class = 'titulo-icone' >Como Fazemos</h4>
+                    </li>  
+                </ul>  
+                
+                <ul class = "menu">
+                    <li class = 'icone'>                              
+                        <input class = "imagem-icone" type="image" src="src\assets\imagens\folha.png"/>
+                        <h4 class = "titulo-icone" >Crescemos</h4>
+                    </li>  
+                    <li class = 'icone'>  
+                        <input class = "imagem-icone" type="image" src="src\assets\imagens\folha.png" />  
+                        <h4 class = 'titulo-icone' >Onde Estamos</h4>
+                    </li>  
+                    <li class = 'icone'>  
+                        <input class = "imagem-icone" type="image" src="src\assets\imagens\folha.png" /> 
+                        <h4 class = 'titulo-icone' >Dicas</h4>
+                    </li>  
+                </ul>          
+            </div>          
         </div>
       </>
     )
