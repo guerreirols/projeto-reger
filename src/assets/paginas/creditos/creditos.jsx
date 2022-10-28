8eaef4797a9447b0481114162f18fe6490f7221d
Guerreiro <luguerreiro.souza@gmail.com>
2022-10-28 16:17:59 -0300

    Ajustes no Rodapé

--- src/assets/paginas/creditos/creditos.jsx
+++ src/assets/paginas/creditos/creditos.jsx
@@ -9,24 +9,28 @@ function Creditos() {
         </style>
 
         <section class="rodape-geral">
-          <div class="rodape">
-              <h1 class="creditos-titulo">Créditos</h1>
-              <div class="desenvolvedores">
-                  <h2 class="dev-titulo">Desenvolvedores</h2>
-                  <h3 class="dev">André Roberto Salvador</h3>
-                  <h3 class="dev">Amanda Pavão Bonito Teixeira</h3>           
-                  <h3 class="dev">Bruno Dias Esquerdo Béu</h3>
+          <div class="rodape">        
+              <div class="desenvolvedores">     
+                <div class="cc">
+                  <h2 class="creditos-titulo">Um site desenvolvido por:</h2> 
+                  <h3 class="c-titulo">Ciência da Computação</h3>
+                  <h3 class="dev">André Roberto Salvador</h3>                
                   <h3 class="dev">Bruno Ventura Santorsula</h3>
                   <h3 class="dev">José Adauto Bolini Junior</h3>
                   <h3 class="dev">Leonardo de Oliveira Salvador</h3>
                   <h3 class="dev">Lucas Guerreiro de Souza</h3>
                   <h3 class="dev">Mateus Oliver Pierim</h3>
+                </div>
+                <div class="design">                
+                  <h3 class="c-titulo">Design</h3>
+                  <h3 class="dev">Amanda Pavão Bonito Teixeira</h3>  
+                  <h3 class="dev">Bruno Dias Esquerdo Béu</h3>
+                  <h2 class="c-titulo">Orientadores</h2>
+                  <h3 class="ori">Vinicius Santos Andrade (Ciência da Computação)</h3>
+                  <h3 class="ori">Sileide Paccola (Design)</h3>
+                </div>                                               
               </div>
-              <div class="orientadores">
-                  <h2 class="ori-titulo">Orientadores</h2>
-                  <h3 class="ori">Vinicius Santos Andrade</h3>
-                  <h3 class="ori">Sileide Paccola</h3>           
-              </div>
+              <img class="reger-logo" src='src\assets\imagens\REGER-CAPA.png'/>
           </div>
         </section>        
       </>
